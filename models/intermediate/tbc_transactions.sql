92477ba024c2ae2ce2a6a058393fd483c8849fb2
Vaso Mtsituri <vasomtsituri@Vasos-MacBook-Pro.local>
2026-01-22 09:19:20 +0400

    feat(transactions): apply change to the operation_type field, instead of just saying card type, now it shows from which bank belongs that card

--- models/intermediate/tbc_transactions.sql
+++ models/intermediate/tbc_transactions.sql
@@ -3,7 +3,7 @@ SELECT
     SAFE_CAST(SPLIT(transaction_description, ',')[SAFE_OFFSET(3)] AS NUMERIC) AS transaction_amount,
     SAFE_CAST(SPLIT(transaction_description, ',')[SAFE_OFFSET(3)] AS NUMERIC) - settlement_amount AS commission,
     settlement_amount,
-    SPLIT(transaction_description, ',')[SAFE_OFFSET(2)] AS operation_type,
+    CONCAT(TRIM(SPLIT(transaction_description, ',')[SAFE_OFFSET(1)]), ' ', TRIM(SPLIT(transaction_description, ',')[SAFE_OFFSET(2)])) AS operation_type,
     rrn,
     'TBC' AS source_bank
 FROM {{ ref("tbc_transactions_stg") }}
